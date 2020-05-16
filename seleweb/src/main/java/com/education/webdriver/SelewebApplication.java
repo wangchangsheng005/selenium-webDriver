b70350a166b08f32710550514354e9c39edfe313
wangchangsheng005 <1577873508@qq.com>
2020-05-16 21:25:51 +0800

    测试版本

---
+++ seleweb/src/main/java/com/education/webdriver/SelewebApplication.java
@@ -0,0 +1,13 @@
+package com.education.webdriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelewebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelewebApplication.class, args);
+    }
+
+}
